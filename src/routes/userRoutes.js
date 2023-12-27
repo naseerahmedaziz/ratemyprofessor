934e707fad4eb2deec0d926266b0d2020424e50b
Naseer Ahmed Aziz <naseerahmedaziz@gmail.com>
2023-12-27 10:55:31 +0500

    findteachers changed into user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const { signup, signin, viewAccount, updateAccount, deleteAccount } = require("../controllers/userController");
+const {
+  signup,
+  signin,
+  viewAccount,
+  updateAccount,
+  deleteAccount,
+} = require("../controllers/userController");
+const { findTeachers } = require("../controllers/adminController");
 const userRouter = express.Router();
+
 //this is working now as well
 userRouter.post("/signup", signup);
 userRouter.post("/signin", signin);
 userRouter.patch("/updateAccount", updateAccount);
+userRouter.get("/findTeachers", findTeachers);
 
 // New route for deleting an account
 // userRouter.get("/account", viewAccount);
 // userRouter.delete("/account", deleteAccount);
 
 module.exports = userRouter;
-
-
